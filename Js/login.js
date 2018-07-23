e3b2bc70512ee535272bb4391d2b37e71893e63f
Alexander Weese <contact@alexweese.de>
2018-07-23 15:59:12 +0200

    ...

--- Js/login.js
+++ Js/login.js
@@ -1,3 +1,54 @@
+var $loading = $('#loading-div').hide();
+var $loginmessage = $('#login-message').hide();
+var $registermessage = $('#register-message').hide();
+var $ajaxMessage = $('#ajax-message').hide();
+$(document)
+        .ajaxStart(function () {
+            $loading.show();
+        })
+        .ajaxStop(function () {
+            $loading.hide();
+            $loginmessage.show();
+            $registermessage.show();
+            $ajaxMessage.show();
+        });
+
+$(document).ready(function () {
+    function getAjaxUrl() {
+        var hostname = document.location.hostname;
+        var url = "https://" + hostname + "/" + "Ajax.php";
+        return url;
+    }
+
+    $('#login-button').bind('click touch', function () {
+        var request = {};
+        request.username = document.getElementById('username-input').value;
+        request.password = document.getElementById('password-input').value;
+        request.action = 'Login';
+        request.tk = '';
+        request.ts = '';
+        request.ipaddress = document.getElementById('session-ipaddress').value;
+        
+        $.ajax({
+            'type': 'POST',
+            'data': {"request": JSON.stringify(request)},
+            'url': getAjaxUrl(),
+            'success': function (data) {
+                if(parseInt(data) == 1) {
+                    window.location.href = '/account';
+                } else {
+                    
+                }
+            },
+            error: function (jqXHR, exception) {
+                $('.ajax-message .text').addClass('error');
+                displayNotice(jqXHR + exception);
+            }
+
+        });
+    });
+
+});
 
 
 
--- Js/user.js
+++ Js/user.js
@@ -1,7 +1,63 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var $loading = $('.loading-div').hide();
+var $loginmessage = $('.login-message').hide();
+var $ajaxMessage = $('.ajax-message').hide();
 
+$(document)
+        .ajaxStart(function () {
+            $loading.show();
+        })
+        .ajaxStop(function () {
+            $loading.hide();
+            $loginmessage.show();
+            $ajaxMessage.show();
+        });
+        
+        
+$(document).ready(function () {
+
+    function getAjaxUrl() {
+        var hostname = document.location.hostname;
+        var url = "https://" + hostname + "/" + "Ajax.php";
+        return url;
+    }
+
+    $('.nav1-link').fancybox();
+
+    $('#createDatasetButton').bind('click touch', function() {
+        var request = {};
+        request.action = 'NewDataset';
+        request.tk = document.getElementById('session-token').value;
+        request.ts = document.getElementById('session-timestamp').value;
+        request.ipaddress = document.getElementById('session-ipaddress').value;
+        request.userID = document.getElementById('datasetUserID').value;
+        request.title = document.getElementById('datasetTitle').value;
+        request.login = document.getElementById('datasetLogin').value;
+        request.password = document.getElementById('datasetPassword').value;
+        request.url = document.getElementById('datasetURL').value;
+        request.project = document.getElementById('datasetProject').value;
+
+        $.ajax({
+            'type': 'POST',
+            'data': {"request": JSON.stringify(request)},
+            'url': getAjaxUrl(),
+            'success': function (data) {
+                if(parseInt(data) == 1) {
+                    window.location.href = '/account';
+                } else {
+                    
+                }
+            },
+            error: function (jqXHR, exception) {
+                $('.ajax-message .text').addClass('error');
+                displayNotice(jqXHR + exception);
+            }
+
+        });
+    });
+
+    $('.dataset').bind('click touch', function() {
+       alert('click');
+    });
+
+});
 
