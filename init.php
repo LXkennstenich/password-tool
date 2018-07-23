b5e71102606df47a6beb30cb1eeff01769c7949e
Alexander Weese <contact@alexweese.de>
2018-07-23 15:57:51 +0200

    Anpassungen für Session

--- init.php
+++ init.php
@@ -4,7 +4,14 @@ if (!defined('PASSTOOL')) {
     die();
 }
 
+
+
+$host = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : $_SERVER['HTTP_HOST'];
+$userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+
 ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
+
 
 include_once 'defines.php';
 
@@ -13,8 +20,13 @@ spl_autoload_register(function($class) {
 });
 
 $factory = new Factory;
-$session = $factory->getSession();
-$sessionStarted = $session->sessionStarted() == true ? true : false;
 
+$session = $factory->getSession();
+$ipAddress = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
+$session->setIpaddress($ipAddress);
+$session->setHost($host);
+$session->setUseragent($userAgent);
+$session->setUsername(isset($_SESSION['U']) ? $_SESSION['U'] : null);
+ob_start();
 
 
