70ab22a43506f30c1f9890cb3d480027c6eaad89
Alexander Weese <contact@alexweese.de>
2018-07-23 15:57:35 +0200

    anpassung weiterleitungen

--- index.php
+++ index.php
@@ -6,25 +6,28 @@ if (!defined('PASSTOOL')) {
     define('PASSTOOL', true);
 }
 
-include_once 'init.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_save_path('/tmp');
+    session_start();
+}
 
 $requestUri = htmlspecialchars(strip_tags(basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))));
 $page = str_replace('/', '', $requestUri);
 
-if ($page == '') {
-    $factory->redirect('login');
-}
+include_once 'init.php';
 
 $file = ROOT_DIR . $page . '.php';
 
 include ELEMENTS_DIR . 'header.php';
+
+
 
 if (file_exists($file)) {
     header("HTTP/1.1 200 OK");
     include $file;
 } else {
     header("HTTP/1.1 404 Not Found");
-    die();
+    $factory->redirect('login');
 }
 
 include ELEMENTS_DIR . 'footer.php';
