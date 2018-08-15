7f4e27f65b8aa948dc038e2123e68af1b95f2c99
Alexander Weese <contact@alexweese.de>
2018-08-15 13:37:42 +0200

    update funktion testen

--- update/updateconfig.php
+++ update/updateconfig.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('PASSTOOL')) {
+    die();
+}
+
 define("TOKEN", "secret-token");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@bitbucket.org:LXkennstenich/password-tool.git"); // The SSH URL to your repository
 define("DIR", ROOT_DIR);                          // The path to your repostiroy; this must begin with a forward slash (/)
