401e1da05bbbf3181449d416d8a5231040028a55
Alexander Weese <contact@alexweese.de>
2018-08-15 20:54:27 +0200

    sampleConfig

---
+++ Config/sampleConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PassTool
+ * @version 1.0
+ * @author Alexander Weese
+ * @package PassTool
+ * @copyright (c) 2018, Alexander Weese
+ */
+if (!defined('PASSTOOL')) {
+    die();
+}
+
+return array(
+    'databaseServer' => 'myserver.com',
+    'databaseName' => 'database',
+    'databaseUser' => 'databaseuser',
+    'databasePassword' => 'databasepassword',
+    'databasePort' => 3306,
+    'adminEmail' => 'test@test.de'
+);
