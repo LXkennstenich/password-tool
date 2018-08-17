9cc909195f1141cc93624ecb26df2c2fd518576e
Alexander Weese <contact@alexweese.de>
2018-08-17 13:27:22 +0200

    update #1

--- Classes/System.php
+++ Classes/System.php
@@ -657,7 +657,7 @@ class System {
                     CURLOPT_URL => $downloadUrl,
                     CURLOPT_RETURNTRANSFER => 1,
                     CURLOPT_FILE => $file,
-                    CURLOPT_TIMEOUT => 50,
+                    CURLOPT_TIMEOUT => 300,
                     CURLOPT_USERAGENT => 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'
                 ]);
             }
